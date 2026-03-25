c1e65495189ff76eb7c8f39bffcb49d0c04786d2
devshanuuu <shanuuu0207@gmail.com>
2026-03-26 01:34:47 +0530

    Passed data of cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -6,23 +6,23 @@ const Cards = (props) => {
     <div className="card">
             
             <div className="top">
-              <img src = "https://1000logos.net/wp-content/uploads/2016/10/Amazon-logo-meaning.jpg" alt = ""/>
+              <img src = {props.logo} alt = ""/>
               <button>Save <Bookmark size ={12}/></button>
             </div>
     
             <div className="center">
               <h3>{props.company} <span>5 days ago</span></h3>
-              <h2>Senior UI/UX Designer</h2>
+              <h2>{props.post}</h2>
               <div className='tag'>
-                <h4>Part Time</h4>
-                <h4>Senior Level</h4>
+                <h4>{props.tag1}</h4>
+                <h4>{props.tag2}</h4>
               </div>
             </div>
     
             <div className="bottom">
               <div>
-                  <h3>$120/hr</h3>
-                  <p>Mumbai, India</p>
+                  <h3>{props.pay}</h3>
+                  <p>{props.location}</p>
               </div>
                 <button>Apply Now</button>
             </div>
